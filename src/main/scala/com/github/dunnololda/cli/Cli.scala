b5c8fc61cf699f3a3ca4990a1a741cf1f3db5460
Andrey Borunov <a.borunov@netris.ru>
2016-03-02 18:07:35 +0300

    v1.8: check property in app file, refactoring

--- src/main/scala/com/github/dunnololda/cli/Cli.scala
+++ src/main/scala/com/github/dunnololda/cli/Cli.scala
@@ -1,14 +1,17 @@
 package com.github.dunnololda.cli
 
-import collection.mutable
+import scala.collection.mutable
 
 trait Cli extends App {
+
   //private val log = Logger(this.getClass.getName)
 
-  case class CliArg(short: String, long: String, description: String, has_value: Boolean, required:Boolean = false)
+  case class CliArg(short: String, long: String, description: String, has_value: Boolean, required: Boolean = false)
 
   private var program_description = ""
+
   def programDescription = program_description
+
   def programDescription_=(descr: String) {
     program_description = descr
   }
@@ -17,14 +20,14 @@ trait Cli extends App {
   private val cli_args_long = mutable.HashMap[String, CliArg]()
   private val cli_args_list = mutable.ArrayBuffer[CliArg]()
 
-  def commandLineArg(short: String, long: String, description: String, has_value: Boolean = false, required:Boolean = false) {
+  def commandLineArg(short: String, long: String, description: String, has_value: Boolean = false, required: Boolean = false) {
     val new_cli_arg = CliArg(short, long, description, has_value, required)
     cli_args_short += (short -> new_cli_arg)
     cli_args_long += (long -> new_cli_arg)
     cli_args_list += new_cli_arg
   }
 
-  def commandLineArgAndParse(short: String, long: String, description: String, has_value: Boolean = false, required:Boolean = false) {
+  def commandLineArgAndParse(short: String, long: String, description: String, has_value: Boolean = false, required: Boolean = false) {
     commandLineArg(short, long, description, has_value, required)
     parseCommandLineArgs()
   }
@@ -32,7 +35,7 @@ trait Cli extends App {
 
   def commandLineArgs(args: (String, String, String, Boolean, Boolean)*) {
     args.foreach {
-      case (short: String, long: String, description: String, has_value: Boolean, required:Boolean) =>
+      case (short: String, long: String, description: String, has_value: Boolean, required: Boolean) =>
         commandLineArg(short, long, description, has_value, required)
     }
   }
@@ -42,25 +45,25 @@ trait Cli extends App {
     parseCommandLineArgs()
   }
 
-  def helpMessage:String = {
+  def helpMessage: String = {
     val result = new StringBuilder()
-    if (program_description != "") result append program_description append  "\n"
+    if (program_description != "") result append program_description append "\n"
     result append "Options:" append "\n"
     cli_args_list.foreach {
       case CliArg(short, long, description, has_value, required) =>
-        val short_only   = (new StringBuilder("-") append short).toString()
-        val except_descr = (new StringBuilder(short_only) append " "*(10 - short_only.length) append "--" append long append
-          (if (has_value) " arg" else "") append (if (required)  " (required)" else "")).toString()
-        result append except_descr append " "*(40 - except_descr.length) append description append "\n"
+        val short_only = (new StringBuilder("-") append short).toString()
+        val except_descr = (new StringBuilder(short_only) append " " * (10 - short_only.length) append "--" append long append
+          (if (has_value) " arg" else "") append (if (required) " (required)" else "")).toString()
+        result append except_descr append " " * (40 - except_descr.length) append description append "\n"
     }
 
     {
-      val short        = "help"
-      val long         = "help"
-      val description  = "show this usage information"
-      val short_only   = (new StringBuilder("-") append short).toString()
-      val except_descr = (new StringBuilder(short_only) append " "*(10 - short_only.length) append "--" append long append " ").toString()
-      result append except_descr append " "*(40 - except_descr.length) append description
+      val short = "help"
+      val long = "help"
+      val description = "show this usage information"
+      val short_only = (new StringBuilder("-") append short).toString()
+      val except_descr = (new StringBuilder(short_only) append " " * (10 - short_only.length) append "--" append long append " ").toString()
+      result append except_descr append " " * (40 - except_descr.length) append description
     }
 
     result.toString()
@@ -84,11 +87,11 @@ trait Cli extends App {
               throw new Exception("failed to start")
             }
             else {
-              def isNextProp(str:String):Boolean = {
+              def isNextProp(str: String): Boolean = {
                 (str.startsWith("--") && cli_args_long.contains(str.replaceFirst("--", ""))) ||
-                (str.startsWith("-") && cli_args_short.contains(str.replaceFirst("-", "")))
+                  (str.startsWith("-") && cli_args_short.contains(str.replaceFirst("-", "")))
               }
-              val value = this.args.drop(pos+1).takeWhile(a => !isNextProp(a)).mkString(" ")
+              val value = this.args.drop(pos + 1).takeWhile(a => !isNextProp(a)).mkString(" ")
               AppProperties.addProperty(long, value, "added command line property")
             }
           }
@@ -103,14 +106,12 @@ trait Cli extends App {
     // перебираем переданные в программу аргументы и заполняем проперти
     this.args.zipWithIndex.filter {
       case (arg, pos) => arg.startsWith("-") && arg != "-"
-    }.map {
-      case (arg, pos) => (arg.toList, pos)
     }.foreach {
       case (arg, pos) =>
-        arg match {
-          case '-' :: '-' :: prop => checkPropInMap(cli_args_long,  prop.mkString, pos)
-          case '-' :: prop        => checkPropInMap(cli_args_short, prop.mkString, pos)
-          case prop               =>
+        arg.toList match {
+          case '-' :: '-' :: prop => checkPropInMap(cli_args_long, prop.mkString, pos)
+          case '-' :: prop => checkPropInMap(cli_args_short, prop.mkString, pos)
+          case prop =>
             println("unknown command line property: " + prop.mkString)
             throw new Exception("failed to start")
         }
@@ -119,9 +120,9 @@ trait Cli extends App {
     // ищем аргументы, которые не переданы в программу, и при этом требуются обязательно
     val missed_required_args = cli_args_list.filter {
       case CliArg(short, long, _, _, required) =>
-        required && !this.args.contains("--" + long) && !this.args.contains("-" + short)
+        required && !this.args.contains("--" + long) && !this.args.contains("-" + short) && !AppProperties.containsProperty(long)
     }
-    if(missed_required_args.nonEmpty) {
+    if (missed_required_args.nonEmpty) {
       missed_required_args.foreach {
         case CliArg(_, long, _, _, _) =>
           println("argument required: " + long)
@@ -130,14 +131,14 @@ trait Cli extends App {
     }
   }
 
-  override def main(args:Array[String]) {
-    if(System.getProperties.containsKey("notrycatch")) {
+  override def main(args: Array[String]) {
+    if (System.getProperties.containsKey("notrycatch")) {
       super.main(args)
     } else {
       try {
         super.main(args)
       } catch {
-        case e:Exception =>
+        case e: Exception =>
           println(e.getLocalizedMessage)
           printHelpAndExit()
       }
